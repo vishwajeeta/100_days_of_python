8fadd57662c45f4c4ef5a426ca24c232b9364f6b
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-07-15 02:38:11 +0530

    modules

---
+++ day2.py
@@ -0,0 +1,15 @@
+"""
+in Replit modules will be installed automatically when we import
+python --version #to check python version
+types of modules
+1.built in modules->shiped with python programming language (internal module)
+eg:- hashlib
+2.external modules->installed using pip->writtern by other peoples
+pip is a package manager
+eg:-pip install pandas
+#<file_Name>.py
+import pandas
+
+#a terminal we see by using python3 or py
+is python REPL [Read Evaluate Print Loop]
+"""
