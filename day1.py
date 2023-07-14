b649fb010aa55cd71e8aea29520a51f54a3e3775
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-07-15 02:19:47 +0530

    introduction to python

---
+++ day1.py
@@ -0,0 +1,10 @@
+"""
+python is a dynamicly typed object orented programming language [oop]
+Guido van Rossum invented python in 1991
+while watching show monty python
+
+--installation--
+install python #in browser ->select os [to run program in local system]
+or
+can use Replit #it's a colaborative browser base ide
+"""
