77dd1bd56210f232c27a02d883f7e4ef5b32f84a
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-09-08 11:55:14 +0530

    checking the datatype

---
+++ day6.py
@@ -0,0 +1,11 @@
+# variables
+a=complex(4,2)
+a1=1
+a2=1.0
+a3=True
+a4=None
+print(f"{a}     {a1}    {a2}    {a3}")
+print(f"{type(a)}\t{type(a1)}\t{type(a2)}\t{type(a3)}\t{type(a4)}")
+
+list1=[1,2,3,[5,6],["vishwa","golu"]]
+print(type(list1))
