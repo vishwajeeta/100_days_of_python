f272af21ec8061e1fed494af4b555e2c6780d000
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-09-08 11:38:32 +0530

    adding serarator and not next line

--- day5.py
+++ day5.py
@@ -1,2 +1,6 @@
 # escape sequence character
 print("hello vishwa.\n how are you \t hope you are fine")
+print("hii i am \"vishwa\"")
+# sererator
+print("hello world !",6,7,sep=" .. ", end=" welcome ")
+print("to the vishwa")
