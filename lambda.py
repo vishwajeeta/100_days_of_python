9ae1d73340a4316966f951870c852d6251e6657c
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-12-22 09:02:15 +0530

    lambda function

---
+++ lambda.py
@@ -0,0 +1,13 @@
+# def add(x,y):
+#     return x+y
+# def sub(x,y):
+#     return x-y
+# def mul(x,y):
+#     return x*y
+
+
+# lambda function can't be used for complex operations is can only be used for single line operations
+add=lambda x,y:x+y
+sub=lambda x,y:x-y
+mul=lambda x,y:x*y
+print(add(5,6),sub(8,4),mul(8,8))
