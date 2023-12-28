76c7ddba6ab890dbe8a4d609f3bd65e69366ecfe
vishwajeeta <gs699542@gmail.com>
2023-12-28 07:43:20 +0530

    filtering the values and getting the values whose's condition is true

---
+++ filters.py
@@ -0,0 +1,12 @@
+#python filter takes two arguments
+#it returns the value if it's true it won't if it's false
+# let's create a list and get the elements which are lower than or equals 5 using filter method
+lis=[1,2,3,4,5,6,7,8,9]
+def condition(x):
+    return x<=5
+
+filt=filter(condition,lis)
+print(type(filt))
+#converting filter datatype to list
+filt=list(filt)
+print(filt)
