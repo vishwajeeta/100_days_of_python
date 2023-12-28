a5d41a7c60d42d85662586a0ce294737bcb487dd
vishwajeeta <gs699542@gmail.com>
2023-12-28 07:47:23 +0530

    tuple, which is immutable

---
+++ tuple.py
@@ -0,0 +1,8 @@
+# tuple is assigned inside parantesees
+# tuple is immuatable datatype it's value can't be change
+tup1=(1,2,3,4,5,6,78)
+print(tup1)
+print(type(tup1))
+print(len(tup1))
+print(f"index of 5 is:{tup1.index(5)}")
+
