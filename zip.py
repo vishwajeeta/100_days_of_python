c2526c085e1cc936359c87f6bf2c204951cb64ec
vishwajeeta <gs699542@gmail.com>
2024-02-10 09:18:40 +0530

    zip function is used to get the data from all the list . of same length

---
+++ zip.py
@@ -0,0 +1,6 @@
+#it will take one one elements from all the list
+a=("vishwa","golu","aman","ajay")
+b=("hii","hello","go to hell","ankit","nihal")
+c=("hii","hello","go to hell","ankit","nihal")
+d=zip(a,b,c)
+print(tuple(d))
